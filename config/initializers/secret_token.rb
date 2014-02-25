9344c12c15ccd25f25dc796ce76d62b86708a961
System Administrator <root@Griffins-MacBook-Pro.local>
2014-02-25 16:38:33 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UffdaSignup::Application.config.secret_key_base = '76e4c1440f42dcd075697e2098fff6fd51b02d56ea1c5eefd7f3a52b0684ad0b76495ecc648d606f17f70ead07f4dbe33b0a0ce6118f3c0a5bc2502d94b2577b'
