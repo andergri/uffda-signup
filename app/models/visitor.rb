0324695134c8840be22315c58af51ee58507016f
Griffin Anderson <andergri1@gmail.com>
2014-02-26 11:56:54 -0500

    assets compiled for Heroku

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,15 +5,13 @@ class Visitor < ActiveRecord::Base
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon::API.new
+    mailchimp = Gibbon::API.new(ENV['MAILCHIMP_API_KEY'])
     result = mailchimp.lists.subscribe({
       :id => ENV['MAILCHIMP_LIST_ID'],
       :email => {:email => self.email},
       :double_optin => false,
       :update_existing => true,
       :send_welcome => true,
-  :enable_starttls_auto => true,
-  :openssl_verify_mode  => 'none'
     })
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
