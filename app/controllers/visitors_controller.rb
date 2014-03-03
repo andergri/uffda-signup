c4d19e931777bad59824644ccbc2765e387a1309
andergri <andergri1@gmail.com>
2014-03-03 17:12:15 -0500

    assets compiled for Heroku

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -8,7 +8,7 @@ class VisitorsController < ApplicationController
     @visitor = Visitor.new(secure_params)
     if @visitor.valid?
       @visitor.subscribe
-      flash[:notice] = "Signed up #{@visitor.email}."
+      flash[:notice] = "Signedup"
       redirect_to root_path
     else
       render :new
